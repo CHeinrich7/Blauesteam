1eebaed10af4edc25790a9386a1cd3eaf6c909ca
Christian Heinrich <cheinrich@silversurfer7.de>
2015-03-19 22:32:44 +0100

    Project added

---
+++ src/cmh/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace cmh\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+
+class DefaultController extends Controller
+{
+
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                Security::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(Security::AUTHENTICATION_ERROR);
+            $session->remove(Security::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('UserBundle:Default:Login.html.php', array(
+            // last username entered by the user
+            'last_username' => $session->get(Security::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+
+    public function logoutAction(Request $request)
+    {
+        $session = $session = $request->getSession();
+        $session->invalidate(0);
+        return $this->redirect($this->generateUrl('user_login'));
+    }
+
+}
---
+++ src/cmh/UserBundle/UserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace cmh\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+}
